8b34cb64d7c26bb96fc3d454db09e0e3033c7719
Jahvon Dockery <jahvondockery@gmail.com>
2025-12-26 16:11:58 -0500

    refactor: remove desktop code (#355)
    The Desktop code is moving to it's own private repo. This PR deletes all
    Tauri application related code. Should have no impact on the existing go
    project

--- desktop/.storybook/decorators.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { MantineProvider } from '@mantine/core';
-import { theme } from '../src/theme';
-
-export const withMantine = (Story: React.ComponentType) => (
-  <MantineProvider theme={theme}>
-    <Story />
-  </MantineProvider>
-);
--- desktop/src/layout/index.ts
+++
@@ -1,3 +0,0 @@
-export { AppShell } from "./AppShell/AppShell";
-export { Header } from "./Header/Header";
-export { Sidebar } from "./Sidebar/Sidebar";
